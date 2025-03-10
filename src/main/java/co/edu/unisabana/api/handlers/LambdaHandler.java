3eeea24485f3b1be1e129142f187a58b0480a8cb
Ana3Lu <anaquiva@unisabana.edu.co>
2025-03-10 01:54:53 -0500

    Agrega manejador Lambda para AWS

---
+++ src/main/java/co/edu/unisabana/api/handlers/LambdaHandler.java
@@ -0,0 +1,41 @@
+package co.edu.unisabana.api.handlers;
+
+import com.amazonaws.serverless.exceptions.ContainerInitializationException;
+import com.amazonaws.serverless.proxy.model.AwsProxyRequest;
+import com.amazonaws.serverless.proxy.model.AwsProxyResponse;
+import com.amazonaws.serverless.proxy.spring.SpringBootLambdaContainerHandler;
+import com.amazonaws.serverless.proxy.spring.SpringBootProxyHandlerBuilder;
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestStreamHandler;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+@SpringBootApplication(scanBasePackages = "co.edu.unisabana.api")
+public class LambdaHandler implements RequestStreamHandler {
+
+    private static final SpringBootLambdaContainerHandler<AwsProxyRequest, AwsProxyResponse> handler;
+
+    static {
+        try {
+            handler = new SpringBootProxyHandlerBuilder<AwsProxyRequest>()
+                    .defaultProxy()
+                    .springBootApplication(LambdaHandler.class)
+                    .buildAndInitialize();
+        } catch (ContainerInitializationException e) {
+            throw new RuntimeException("Error al inicializar la aplicación en AWS Lambda", e);
+        }
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(LambdaHandler.class, args);
+    }
+
+    @Override
+    public void handleRequest(InputStream inputStream, OutputStream outputStream, Context context) throws IOException {
+        handler.proxyStream(inputStream, outputStream, context);
+    }
+}
